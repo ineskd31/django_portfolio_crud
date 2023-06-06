760e0bba35d1cb43d4a94a3bf6517d3f55edcd0f
ineskd31 <ines.kaddi@hotmail.fr>
2023-06-06 22:06:42 +0200

    edit about done

--- app_about/views.py
+++ app_about/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import About
+from .forms import AboutForm
 
 
 # Create your views here.
+def edit(request, id):
+    edit = About.objects.get(id=id)
+    if request.method == 'POST':
+        form = AboutForm(request.POST, instance=edit)
+        if form.is_valid():
+            form.save()
+            return redirect('homeback')
+    else:
+        form = AboutForm(instance=edit)
+        return render(request, 'temp/backoffice/about/edit.html', {'form':form})
--- projet_portfolio_crud/urls.py
+++ projet_portfolio_crud/urls.py
@@ -19,9 +19,12 @@ from django.urls import path
 
 from app_home.views import *
 from app_back_home.views import *
+from app_about.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='homefront'),
-    path('backhome/', backhome, name="homeback" )
+    path('backhome/', backhome, name="homeback" ),
+    path('edit/<int:id>', edit, name='edit'),
+    
 ]
