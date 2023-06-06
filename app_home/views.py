357a21ca00f7473fab056f69a5f7a5eb7f0d8168
ineskd31 <ines.kaddi@hotmail.fr>
2023-06-06 20:20:02 +0200

    about affichage done

--- app_home/views.py
+++ app_home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from app_about.models import About
 
 # Create your views here.
 def home(request):
-    return render(request, 'temp/home/home.html')
+    allabout = About.objects.all()
+    return render(request, 'temp/home/home.html', {"allabout": allabout})
